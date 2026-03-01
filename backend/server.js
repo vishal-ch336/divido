db920bc4f88756d082becb5bd5623be0fcf477cc
vishal-ch336 <vishalchipurupalli336@gmail.com>
2026-03-01 15:11:23 +0530

    fix: handle cors preflight

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use(
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
-
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
